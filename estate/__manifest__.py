9f3a01258ea38053ae7986a071b37b2a29528ea9
Moaz Mohamed <moazmesmail@gmail.com>
2025-06-23 22:57:19 +0300

    Feat: estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "estate",
+    "author":"Moaz Mohamed",
+    "depends":[ 'base' ],
+    "data" : [
+        "views/base_menu.xml",
+        "views/estate_tree_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "application":True
+}
---
+++ estate/models/estate_model.py
@@ -0,0 +1,32 @@
+from odoo import models,fields
+
+class estate(models.Model):
+    _name = "estate"
+
+    name=fields.Char()
+    state=fields.Selection([
+        ("new","New"),
+        ("canceled", "Canceled"),
+    ])
+    description=fields.Text()
+    postcode=fields.Char()
+    date_availability=fields.Date()
+    expected_price=fields.Integer()
+    selling_price=fields.Integer()
+    bedrooms=fields.Integer()
+    living_area=fields.Float()
+    facades=fields.Integer()
+    garage=fields.Boolean()
+    garden=fields.Boolean()
+    garden_area=fields.Float()
+    garden_orientation=fields.Selection(
+        [
+            ("south","South"),
+            ("north", "North"),
+            ("east", "East"),
+            ("west","West"),
+        ]
+    )
+
+
+
